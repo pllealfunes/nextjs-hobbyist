089db2c7e643edd25a5efda09510386993460fdc
pllealfunes <51715816+pllealfunes@users.noreply.github.com>
2024-09-30 20:56:14 -0400

    made nav responsive on mobile

--- src/components/ui/nav.tsx
+++ src/components/ui/nav.tsx
@@ -1,63 +1,67 @@
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Nav() {
   return (
-    <div>
-      <header className="bg-zinc-50">
-        <nav
-          aria-label="Global"
-          className="flex items-center justify-between p-6 lg:px-8"
-        >
-          <div className="flex lg:flex-1">
-            <div className="flex justify-center items-center">
-              <Link
-                href="/"
-                className="-m-1.5 p-1.5"
-                aria-label="Go to homepage"
-              >
-                <span className="sr-only">Hobbyist</span>
-                <Image
-                  alt="Logo"
-                  src="/images/feather.svg"
-                  height={36}
-                  width={36}
-                  className="h-9 w-auto"
-                />
-              </Link>
-              <p
-                className="text-rose-300 m-2 text-2xl font-semibold"
-                aria-hidden="true"
-              >
-                Hobbyist
-              </p>
-            </div>
-          </div>
-          <div className="flex gap-x-6 items-center">
+    <header className="bg-zinc-50">
+      <nav
+        aria-label="Global"
+        className="flex items-center justify-between p-4 sm:p-6 lg:px-8"
+      >
+        <div className="flex lg:flex-1">
+          <div className="flex items-center space-x-2 sm:space-x-4">
+            {/* Logo and text */}
             <Link
-              href="#"
-              className="font-semibold leading-6 text-gray-900"
-              aria-label="Explore content"
+              href="/"
+              className="-m-1.5 p-1.5 hidden sm:inline-block" // Hide logo on mobile, show from small screen
+              aria-label="Go to homepage"
             >
-              Explore
+              <span className="sr-only">Hobbyist</span>
+              <Image
+                alt="Logo"
+                src="/images/feather.svg"
+                height={36}
+                width={36}
+                className="h-9 w-auto"
+              />
             </Link>
             <Link
-              href="#"
-              className="bg-rose-500 shadow-rose-300 hover:bg-rose-600 text-white font-semibold py-2 px-4 rounded-full transition duration-300 leading-6 shadow-rose-300 shadow-lg"
-              aria-label="Login to your account"
+              href="/"
+              className="text-rose-300 text-2xl sm:text-3xl md:text-2xl lg:text-2xl font-semibold"
+              aria-hidden="true"
             >
-              Login
-            </Link>
-            <Link
-              href="#"
-              className="font-semibold leading-6 text-gray-900"
-              aria-label="Sign up for an account"
-            >
-              Signup
+              Hobbyist
             </Link>
           </div>
-        </nav>
-      </header>
-    </div>
+        </div>
+
+        {/* Navigation links */}
+        <div className="flex gap-x-4 sm:gap-x-6 items-center">
+          <Link
+            href="#"
+            className="font-semibold leading-6 text-gray-900"
+            aria-label="Explore content"
+          >
+            Explore
+          </Link>
+
+          <Link
+            href="#"
+            className="bg-rose-500 hover:bg-rose-600 text-white font-semibold py-2 px-3 sm:px-4 rounded-full transition duration-300 leading-6 shadow-rose-300 shadow-lg"
+            aria-label="Login to your account"
+          >
+            Login
+          </Link>
+
+          <Link
+            href="#"
+            className="hidden sm:block font-semibold leading-6 text-gray-900"
+            aria-label="Sign up for an account"
+          >
+            Signup
+          </Link>
+        </div>
+      </nav>
+    </header>
   );
 }
