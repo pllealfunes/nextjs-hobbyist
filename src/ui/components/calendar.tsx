b392cc518040a1cbbdfce044b8fe0921a8fd1374
pllealfunes <51715816+pllealfunes@users.noreply.github.com>
2024-10-08 00:26:19 -0400

    removed unnecessary code from calendar.tsx

--- src/ui/components/calendar.tsx
+++ src/ui/components/calendar.tsx
@@ -1,15 +1,13 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css"; // Import the default styles
 
 const CalendarComponent = () => {
-  const [value, setValue] = useState(new Date());
-
   return (
     <div className="calendar-container">
-      <Calendar value={value} />
+      <Calendar />
     </div>
   );
 };
