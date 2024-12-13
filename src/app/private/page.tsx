56a1ed50ffd1cece35ed1ed59f1be9b36e4b0548
pllealfunes <51715816+pllealfunes@users.noreply.github.com>
2024-12-13 17:29:59 -0500

    removed supabase code from private view file

--- src/app/private/page.tsx
+++ src/app/private/page.tsx
@@ -1,16 +1,3 @@
-import { redirect } from "next/navigation";
-
-import { createClient } from "@/utils/supabase/server";
-
 export default async function PrivatePage() {
-  const supabase = await createClient();
-
-  const { data, error } = await supabase.auth.getUser();
-  console.log(data);
-
-  if (error || !data?.user) {
-    redirect("/login");
-  }
-
-  return <p>Hello {data.user.email}</p>;
+  return <p>Hello</p>;
 }
