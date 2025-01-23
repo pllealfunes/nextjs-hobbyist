7d9115d48e4c12d3fef0f819e7d42a82a16de77c
pllealfunes <51715816+pllealfunes@users.noreply.github.com>
2025-01-22 22:56:38 -0500

    removed unused code, added .log

--- src/app/api/categories/route.tsx
+++ src/app/api/categories/route.tsx
@@ -1,12 +1,14 @@
 // pages/api/categories.ts
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   try {
     const categories = await prisma.category.findMany();
     return NextResponse.json(categories, { status: 200 });
   } catch (error) {
+    console.log(error);
+
     return NextResponse.json(
       { error: "Error fetching user data" },
       { status: 500 }
