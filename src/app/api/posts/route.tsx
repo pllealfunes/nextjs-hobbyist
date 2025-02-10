d49c49738b3d9f5d49ae1e0c3d1cc20a89455418
pllealfunes <51715816+pllealfunes@users.noreply.github.com>
2025-02-09 22:15:41 -0500

    commented code in /api/posts

--- src/app/api/posts/route.tsx
+++ src/app/api/posts/route.tsx
@@ -1,5 +1,5 @@
-import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+//import prisma from "@/lib/prisma";
 import { createClient } from "@/utils/supabase/client";
 
 const supabase = createClient();
