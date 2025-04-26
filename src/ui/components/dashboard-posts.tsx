c77bab0397a0cac2ff4ccd25604cdc8ec19efaba
pllealfunes <51715816+pllealfunes@users.noreply.github.com>
2025-04-25 23:28:55 -0400

    updated post card layout based off coverphoto is null or not

--- src/ui/components/dashboard-posts.tsx
+++ src/ui/components/dashboard-posts.tsx
@@ -26,19 +26,17 @@ export default function DashboardPosts({ post, categories }: PostCardProps) {
   return (
     <div className="bg-white rounded-lg shadow-lg overflow-hidden transition-transform transform hover:scale-105 duration-300 cursor-pointer">
       {/* Card Image */}
-      <div className="relative">
-        <Image
-          src={
-            post.coverphoto && post.coverphoto.trim() !== ""
-              ? post.coverphoto
-              : "/default-image.jpg"
-          }
-          alt={post.title}
-          width={800}
-          height={400}
-          className="w-full object-cover rounded-lg"
-        />
-      </div>
+      {post.coverphoto && post.coverphoto.trim() !== "" && (
+        <div className="relative">
+          <Image
+            src={post.coverphoto}
+            alt={post.title}
+            width={800}
+            height={400}
+            className="w-full object-cover rounded-lg"
+          />
+        </div>
+      )}
 
       {/* Card Content */}
       <div className="p-5">
