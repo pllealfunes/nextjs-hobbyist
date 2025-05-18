e3f1827117f23d75d5ffca349a98f81aaa9bb159
pllealfunes <51715816+pllealfunes@users.noreply.github.com>
2025-05-18 19:57:06 -0400

    changed supabase import in usersettings to point to client.ts

--- src/app/usersettings/page.tsx
+++ src/app/usersettings/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { createClient } from "@/utils/supabase/server";
+import { createClient } from "@/utils/supabase/client";
 import {
   changeEmail,
   changePassword,
